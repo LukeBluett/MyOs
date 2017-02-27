0d9ce4b8b0737045f5967cb9e4507c379b37d8dd
Luke Bluett <LukeBluett@mail.com>
2017-02-27 06:24:50 +0000

    Modified File, Added function
    modified:   kernel.cpp
    
    Added function printf in kernel.cpp to print text to the screen

--- kernel.cpp
+++ kernel.cpp
@@ -1,4 +1,12 @@
 
+void printf(char* str)
+{
+	unsigned short* VideoMemory = (unsigned short*)0xb8000;
+
+	for(int i = 0; str[i] != '\0'; i++)
+		VideoMemory[i] = (VideoMemory[i] & 0xF00) | str[i];
+}
+
 void kernelMain(void* multiboot_structure, unsigned int magicnumber)
 {
 	printf("Hello, World!");
